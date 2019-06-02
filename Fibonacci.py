a9876c5322a6b7e4d698e9df4a4061f22d824c30
kartik-soni1707 <its.kartiksoni@gmail.com>
2019-06-02 21:00:14 +0530

    Update Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,15 +2,18 @@ import sys
 sys.setrecursionlimit(3296)#Incraesing recursion limit
 
 def fib(num):
-    a=0
-    b=1
-    print(a)
-    print(b)
-    for i in range(2,num):
-        c=a+b
-        a=b
-        b=c
-        print(c)
+    if num==1:
+        print(a)
+    else:
+        a=0
+        b=1
+        print(a)
+        print(b)
+        for i in range(2,num):
+            c=a+b
+            a=b
+            b=c
+            print(c)
 
 x=int(input('Enter no.:'))
 fib(x)
