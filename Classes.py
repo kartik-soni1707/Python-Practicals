e5a6c28f4814214d5928b5ce2284ca97e83bf13e
kartik-soni1707 <its.kartiksoni@gmail.com>
2019-06-13 11:31:50 +0530

    Add files via upload

---
+++ Classes.py
@@ -0,0 +1,17 @@
+class School:
+    loc="India"#class var
+    def __init__(self):
+        print('This is a constructor')
+    def clss(self):#Instance method
+        sec='A'#Instance var
+
+    @classmethod #class method
+    def prncp(cls):
+        princi='Mrs.X'
+
+    @staticmethod#Static method
+    def Hmd():
+        print("Blah blah")
+
+dps=School()
+
